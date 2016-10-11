57c1798e194a6784c34b28c5f593314053a70eda
Mark Ireland <mark.ireland@nearform.com>
2016-10-11 10:25:32 +0100

    Changed PK IDs to just be 'id', for ease of use in UI and to save mapping them at API

--- scripts/init/database/migrations/001.do.sql
+++ scripts/init/database/migrations/001.do.sql
@@ -1,14 +1,15 @@
 
 /* TODO: consider using unique non-sequential ids, for security
   (less predictabiity)
+  probably GUID
  */
 CREATE TABLE users (
-  user_id   SERIAL UNIQUE,
+  id        SERIAL UNIQUE,
   name      VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE policies (
-  policy_id SERIAL UNIQUE,
+  id        SERIAL UNIQUE,
   name      VARCHAR(30) NOT NULL,
   effect    VARCHAR(30) NOT NULL,
   action    VARCHAR(30) NOT NULL,
@@ -16,30 +17,30 @@ CREATE TABLE policies (
 );
 
 CREATE TABLE organizations (
-  org_id      SERIAL UNIQUE,
+  id          SERIAL UNIQUE,
   name        VARCHAR(30) NOT NULL,
   description VARCHAR(30)
 );
 
 CREATE TABLE teams (
-  team_id         SERIAL UNIQUE,
+  id              SERIAL UNIQUE,
   name            VARCHAR(30) NOT NULL,
   description     VARCHAR(90),
-  team_parent_id  INT REFERENCES teams(team_id),
-  org_id          INT REFERENCES organizations(org_id) NOT NULL
+  team_parent_id  INT REFERENCES teams(id),
+  org_id          INT REFERENCES organizations(id) NOT NULL
 );
 
 CREATE TABLE team_members (
-  team_id  INT REFERENCES teams(team_id) NOT NULL,
-  user_id  INT REFERENCES users(user_id) NOT NULL
+  team_id  INT REFERENCES teams(id) NOT NULL,
+  user_id  INT REFERENCES users(id) NOT NULL
 );
 
 CREATE TABLE user_policies (
-  user_id   INT REFERENCES users(user_id) NOT NULL,
-  policy_id INT REFERENCES policies(policy_id) NOT NULL
+  user_id   INT REFERENCES users(id) NOT NULL,
+  policy_id INT REFERENCES policies(id) NOT NULL
 );
 
 CREATE TABLE team_policies (
-  team_id   INT REFERENCES teams(team_id) NOT NULL,
-  policy_id INT REFERENCES policies(policy_id) NOT NULL
+  team_id   INT REFERENCES teams(id) NOT NULL,
+  policy_id INT REFERENCES policies(id) NOT NULL
 );
