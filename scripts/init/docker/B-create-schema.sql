4cec3af45b2c61cd91460364ed7d09242a7d7ca2
Michele Capra <michelecapra83@gmail.com>
2016-10-12 15:05:01 +0200

    fixed authorization db creation in postgres container

---
+++ scripts/init/docker/B-create-schema.sql
@@ -0,0 +1,4 @@
+CREATE DATABASE "authorization";
+
+-- DROP ROLE admin;
+CREATE ROLE "admin" LOGIN PASSWORD 'default';
--- scripts/init/docker/dbschema.sql
+++
@@ -1,5 +0,0 @@
-DROP DATABASE IF EXISTS "authorization";
-CREATE DATABASE "authorization";
-
--- DROP ROLE admin;
-CREATE ROLE "admin" LOGIN PASSWORD 'default';
