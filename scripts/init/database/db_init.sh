9a1140d9c78945834a17f3af70324210fa2c8149
Michele Capra <michelecapra83@gmail.com>
2016-10-13 15:58:24 +0200

    removed database distruction and recreation srcipt from docker image

--- scripts/init/database/db_init.sh
+++ scripts/init/database/db_init.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-# remove old database, if it exists
-node scripts/init/database/install_000.js;
+PGPASSWORD=postgres dropdb -h localhost -p 5432 -U postgres authorization
+PGPASSWORD=postgres createdb -h localhost -p 5432 -U postgres authorization
+
+PGPASSWORD=postgres psql -h localhost -p 5432 -U postgres --echo-all -c "DROP USER IF EXISTS admin;"
+PGPASSWORD=postgres psql -h localhost -p 5432 -U postgres --echo-all -c "CREATE USER admin WITH PASSWORD 'default';"
 # install new database
 node scripts/init/database/install_001.js;
 
