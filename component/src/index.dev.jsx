0b726e41c89e6fd0036890d92e6a361b297ef714
Salman Mitha <salmanmitha@gmail.com>
2016-10-14 16:02:21 +0530

    Added nav bar

--- component/src/index.dev.jsx
+++ component/src/index.dev.jsx
@@ -6,6 +6,7 @@ import injectTapEventPlugin from 'react-tap-event-plugin'
 import Routes from './Routes'
 
 import 'muicss/lib/sass/mui.scss'
+import './main.scss'
 
 // Needed for onTouchTap
 // http://stackoverflow.com/a/34015469/988941
