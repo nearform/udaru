be6bf3310f106aa60e5f1f6554da266b30082a31
Filippo De Santis <filippo.desantis@gmail.com>
2016-11-21 11:26:49 +0100

    Runs psql commands in docker container. No need for psql client

--- scripts/init/database/db_init.sh
+++ scripts/init/database/db_init.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-PGPASSWORD=postgres dropdb -h localhost -p 5432 -U postgres authorization
-PGPASSWORD=postgres createdb -h localhost -p 5432 -U postgres authorization
+docker exec -it labsauthorization_database_1 bash -c "PGPASSWORD=postgres dropdb -h localhost -p 5432 -U postgres authorization"
+docker exec -it labsauthorization_database_1 bash -c "PGPASSWORD=postgres createdb -h localhost -p 5432 -U postgres authorization"
 # by default the db has UTF8 character encoding with auto-commit
 
-PGPASSWORD=postgres psql -h localhost -p 5432 -U postgres --echo-all -c "DROP USER IF EXISTS admin;"
-PGPASSWORD=postgres psql -h localhost -p 5432 -U postgres --echo-all -c "CREATE USER admin WITH PASSWORD 'default';"
+docker exec -it labsauthorization_database_1 bash -c "PGPASSWORD=postgres psql -h localhost -p 5432 -U postgres --echo-all -c \"DROP USER IF EXISTS admin;\""
+docker exec -it labsauthorization_database_1 bash -c "PGPASSWORD=postgres psql -h localhost -p 5432 -U postgres --echo-all -c \"CREATE USER admin WITH PASSWORD 'default';\""
 # install new database
 node scripts/init/database/install_001.js;
--- scripts/init/database/db_load.sh
+++ scripts/init/database/db_load.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 # TODO: team_members, team_policies
 # TODO: statement_*
-PGPASSWORD=postgres psql -h localhost -p 5432 -U postgres -d authorization <<EOF
+docker cp ./scripts/init/database/testdata labsauthorization_database_1:/testdata
+docker exec -it labsauthorization_database_1 bash -c "PGPASSWORD=postgres psql -h localhost -p 5432 -U postgres -d authorization <<EOF
 SELECT 'Database installed, schemaversion = ' || MAX(version) from schemaversion;
-\cd './scripts/init/database/testdata'
+\cd '/testdata'
 \! pwd
 \COPY organizations(id, name, description) FROM 'organizations.csv' (FORMAT csv)
 \COPY users(name, org_id) FROM 'users.csv' (FORMAT csv)
 \COPY teams(name, description, team_parent_id, org_id) FROM 'teams.csv' (FORMAT csv)
 \COPY team_members(user_id, team_id) FROM 'team_members.csv' (FORMAT csv)
-EOF
+EOF"
 node scripts/init/database/testdata/loadPolicies.js
-PGPASSWORD=postgres psql -h localhost -p 5432 -U postgres -d authorization <<EOF
-\cd './scripts/init/database/testdata'
+docker exec -it labsauthorization_database_1 bash -c "PGPASSWORD=postgres psql -h localhost -p 5432 -U postgres -d authorization <<EOF
+\cd './testdata'
 \! pwd
 \COPY ref_actions(action) FROM 'ref_actions.csv' (FORMAT csv)
 \COPY user_policies(user_id, policy_id) FROM 'user_policies.csv' (FORMAT csv)
 \COPY team_policies(team_id, policy_id) FROM 'team_policies.csv' (FORMAT csv)
-EOF
+EOF"
