69155f2b2c9f387480521493b44969ebc4e0cc35
Mihai Dima <mihaizn@gmail.com>
2017-02-28 18:13:40 +0200

    test udaru resources configured in env

--- test/udaru.env.test.js
+++ test/udaru.env.test.js
@@ -21,4 +21,19 @@ lab.experiment('test config env variable', () => {
       done()
     })
   })
+
+  lab.test('set resources in env and check Udaru uses it', (done) => {
+    const testTemplate = '/{{ namespace }}/organization1/[organizationId]'
+    const options = {
+      env: Object.assign({}, process.env, { UDARU_RESOURCES_templates_organizations: testTemplate })
+    }
+    const jsCode = 'node -e \'const config = require("./lib/core/config/config.auth.js"); console.log(config.resources.organizations({organizationId: 1}))\''
+
+    exec(jsCode, options, (error, stdout, stderr) => {
+      expect(error).to.not.exist()
+      expect(stdout.slice(0, stdout.length - 1)).to.equal('/authorization/organization1/1')
+
+      done()
+    })
+  })
 })
