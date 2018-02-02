68c5cc760e86805bb0a8d2c7cdf1a95dd4ebf47d
Mihai Dima <mihai.dima@nearform.com>
2018-02-02 17:07:33 +0200

    feat: get user teams, no inheritance

--- test/integration/userOps.test.js
+++ test/integration/userOps.test.js
@@ -666,3 +666,69 @@ lab.experiment('UserOps', () => {
     })
   })
 })
+
+lab.experiment('UserOps structure', () => {
+  lab.test('Validate existing test teams', (done) => {
+    udaru.teams.list({ organizationId: 'WONKA' }, (err, result) => {
+      expect(err).to.not.exist()
+      expect(result).to.exist()
+      let expectedTeams = [
+        'Admins',
+        'Readers',
+        'Authors',
+        'Managers'
+      ]
+      expect(_.map(result, 'name')).contains(expectedTeams)
+
+      done()
+    })
+  })
+
+  lab.test('Validate existing org user', (done) => {
+    udaru.users.list({ organizationId: 'WONKA' }, (err, result) => {
+      expect(err).to.not.exist()
+      expect(result).to.exist()
+      let expectedUsers = [
+        'VerucaId'
+      ]
+      expect(_.map(result, 'id')).contains(expectedUsers)
+
+      done()
+    })
+  })
+
+  lab.test('Test user exists in two teams, no pagination', (done) => {
+    udaru.users.listUserTeams({ id: 'VerucaId', organizationId: 'WONKA' }, (err, result) => {
+      expect(err).to.not.exist()
+      expect(result).to.exist()
+      let expectedTeams = [
+        'Readers',
+        'Authors'
+      ]
+      expect(_.map(result.data, 'name')).contains(expectedTeams)
+      expect(result.page).to.equal(1)
+      expect(result.limit).to.equal(2)
+      expect(result.total).to.equal(2)
+      expect(result.data.length).to.equal(2)
+
+      done()
+    })
+  })
+
+  lab.test('Test user exists in two teams, pagination', (done) => {
+    udaru.users.listUserTeams({ id: 'VerucaId', organizationId: 'WONKA', page: 2, limit: 1 }, (err, result) => {
+      expect(err).to.not.exist()
+      expect(result).to.exist()
+      let expectedTeams = [
+        'Authors'
+      ]
+      expect(_.map(result.data, 'name')).contains(expectedTeams)
+      expect(result.page).to.equal(2)
+      expect(result.limit).to.equal(1)
+      expect(result.total).to.equal(2)
+      expect(result.data.length).to.equal(1)
+
+      done()
+    })
+  })
+})
