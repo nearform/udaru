9e218149f0bc4073eb7bc001c5021e834b9eb41e
Osmond van Hemert <osmond@vanhemert.nu>
2019-06-06 14:11:18 +0200

    fix test runs on node 12

--- packages/udaru-hapi-16-plugin/test/edgeCases.test.js
+++ packages/udaru-hapi-16-plugin/test/edgeCases.test.js
@@ -40,6 +40,7 @@ lab.experiment('Edge cases', () => {
     buildServer({}, err => {
       expect(err).to.exist()
       require.cache[resolved] = null
+      delete require.cache[resolved]
       done()
     })
   })
